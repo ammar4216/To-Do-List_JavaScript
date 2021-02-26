c67b11505aa1b111a438f1f32ccaf854b64aa650
ammar4216 <aym4216@gmail.com>
2021-02-26 16:26:50 +0500

    to do list files

---
+++ app.js
@@ -0,0 +1,45 @@
+
+const input = document.querySelector('input');
+const addBtn = document.querySelector('.addTask > button');
+
+addBtn.addEventListener('click', addList);
+
+addBtn.addEventListener('keyup', (e)=> {
+    (e.keyCode == 15 ? addList(e) : null );
+});
+
+function addList(e){
+    const notCompleted = document.querySelector('.notCompleted');
+    const completed = document.querySelector('.completed');
+
+    const newLi = document.createElement('li');
+    const checkBtn = document.createElement('button');
+    const delBtn = document.createElement('button');
+
+    checkBtn.innerHTML = '<i class="fa fa-check"></i>';
+    delBtn.innerHTML = '<i class="fa fa-trash"></i>';
+    
+
+    if(input.value !==''){
+        newLi.textContent = input.value;
+        input.value = '';
+        notCompleted.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(delBtn);
+    }
+
+    checkBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+        completed.appendChild(parent);
+        checkBtn.style.display = 'none';
+    });
+
+    delBtn.addEventListener('click', function(){
+        const parent = this.parentNode;
+        parent.remove();
+        
+    });
+    
+}
+
